ffc085a52bf3e952e6003991756b010bf7cade25
m-fatahillah <fatahcrown789@gmail.com>
2026-03-09 05:12:46 +0700

    praktikum 2.1

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -9,16 +9,10 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index()
-    {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345') // Perbaikan: hapus spasi setelah ::
-        ];
-
-        UserModel::create($data);
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
+{
+    $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        abort(404);
+    });
+    return view('user', ['data' => $user]);
+}
 }
