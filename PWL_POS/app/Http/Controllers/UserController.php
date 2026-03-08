276861415faa5b4642bbf3a814e95d74364cd726
m-fatahillah <fatahcrown789@gmail.com>
2026-03-09 04:01:50 +0700

    praktikum 1 $fillable

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -2,24 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\UserModel;
+use App\Models\UserModel; // Tambahkan ini
+use Illuminate\Support\Facades\Hash; // Tambahkan ini
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345') // Perbaikan: hapus spasi setelah ::
         ];
 
-        UserModel::where('username', 'customer-1')->update($data); // update data user
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-
+        UserModel::create($data);
+        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -9,9 +9,13 @@ class UserModel extends Model
 {
     use HasFactory;
 
-    // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $table = 'm_user';
-
-    // Mendefinisikan primary key dari tabel yang digunakan
     protected $primaryKey = 'user_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
