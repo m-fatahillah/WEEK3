f4385ab7480c334216cfb0e097c8fd73a5bbdf57
m-fatahillah <fatahcrown789@gmail.com>
2026-03-09 06:33:58 +0700

    praktikum 2.2

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -9,10 +9,8 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index()
-{
-    $user = UserModel::findOr(20, ['username', 'nama'], function () {
-        abort(404);
-    });
-    return view('user', ['data' => $user]);
-}
+    {
+        $user = UserModel::where('username', 'manager9')->firstOrFail(); // [cite: 203]
+        return view('user', ['data' => $user]);
+    }
 }
